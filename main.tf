abffaaae045091575483fd773017ae1161b5259c
stefge-ca <69005017+stefge-ca@users.noreply.github.com>
2020-07-30 11:27:35 -0400

    Updated my code.

--- main.tf
+++ main.tf
@@ -117,7 +117,7 @@ resource "azurerm_virtual_machine" "catapp" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
-  tags {
+  tags = {
     Billable = true,
     Department = "devops"
 }
